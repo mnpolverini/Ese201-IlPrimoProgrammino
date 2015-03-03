489e85c45af671298b105903f7094b1f3e84ce75
ManuPolv <manupolv@icloud.com>
2015-03-03 23:27:05 +0100

    exMedia

---
+++ exmedia.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+main()
+{
+      int n1,n2;
+      float media;
+      
+      printf("Inserisci il primo numero: ");
+      scanf("%d",&n1);
+      
+      printf("Inserisci il secondo numero: ");
+      scanf("%d",&n2);
+      
+      media=n1+n2;
+      media=media/2;
+      
+      printf("La media e':  %f", media);
+          
+printf("\n\n");
+
+      
+}
---
+++ exmedia.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "inserisci il primo numero"
+read n1
+echo "inserisci il secondo numero"
+read n2
+
+media=$(((n1+n2)/2))
+echo
+echo "La media e': $media"
+
+
